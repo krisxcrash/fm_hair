267c066205652510ce5fb920703fc7a7136efeb7
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-13 22:21:47 -0800

    updated server to handle multiple routes, added additional html pages

--- server.js
+++ server.js
@@ -6,11 +6,52 @@ var PORT = 8080
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-    fs.readFile(__dirname + "/index.html", function(err, data) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(data);
-    });
 
+    var path = req.url;
+
+    switch (path) {
+        case "/":
+        fs.readFile(__dirname + "/index.html", function(err, data) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+        break;
+
+        case "/about":
+        fs.readFile(__dirname + "/about.html", function(err, data) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+        break;
+
+        case "/contact":
+        fs.readFile(__dirname + "/contact.html", function(err, data) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+        break;
+
+        case "/gallery":
+        fs.readFile(__dirname + "/gallery.html", function(err, data) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+        break;
+
+        case "/scheduling":
+        fs.readFile(__dirname + "/scheduling.html", function(err, data) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+        break;
+
+        default: 
+        fs.readFile(__dirname + "/index.html", function(err, data) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+          });
+        break;
+    }
 }
 
 server.listen(PORT, function() {
